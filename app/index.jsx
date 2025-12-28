846e2e23195a10a0b63ab52b7b4aab8a7c73183b
NesmaFarid9 <nesmafarid45@gmail.com>
2025-12-28 03:07:51 +0200

    Implemented the payment method cards clickable

--- app/index.jsx
+++ app/index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import BookingScreen from '../components/booking/booking';
 import SearchResultsScreen from '../components/search/SearchResultsScreen';
 import ReservationScreen from '../components/reservation/ReservationScreen';
+import PaymentScreen from '../components/payment/PaymentScreen';
 
 export default function HomeScreen() {
   // return <ReservationScreen/>
-  return <BookingScreen />;
+  return <PaymentScreen/>
+  // return <BookingScreen />;
 }
