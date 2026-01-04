477ed12c1c50f492720a5d660e8caf8e0cf2cd3b
mahmoudAmagdy <58373205+mahmoudAmagdy@users.noreply.github.com>
2026-01-04 21:35:38 +0200

    (icon) added ios and android icons (app) config for ios and android

--- app.config.js
+++ app.config.js
@@ -1,22 +1,31 @@
 
 export default {
   expo: {
-    name: 'NewBassamShipping',
-    slug: 'NewBassamShipping',
+    name: 'BassamBooking',
+    slug: 'bassambooking',
     version: '1.0.0',
     orientation: 'portrait',
-    scheme: 'bassamshipping',
+    scheme: 'bassambooking',
     userInterfaceStyle: 'light',
 
     assetBundlePatterns: ['**/*'],
-
+    icon: './assets/icon.png',
     ios: {
+      icon: './assets/icon.png',
       supportsTablet: true,
+      infoPlist: {
+        // UIBackgroundModes: ['fetch', 'remote-notification'],
+        ITSAppUsesNonExemptEncryption: false,
+      },
     },
 
     android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/icon.png',
+        backgroundColor: '#FFFFFF',
+      },
       permissions: ['android.permission.DETECT_SCREEN_CAPTURE'],
-      package: 'com.zeina.hass.bassamshippingfrontend',
+      package: 'com.bassamshippingsa.bassambooking',
     },
     plugins: [
       'expo-router',
@@ -31,11 +40,11 @@ export default {
     ],
     android: {
       useNextNotificationsApi: true,
-      package: "com.yourcompany.bassamshipping"
+      package: "com.bassamshippingsa.bassambooking",
     },
     ios: {
       supportsTablet: true,
-      bundleIdentifier: "com.yourcompany.bassamshipping",
+      bundleIdentifier: "com.bassamshippingsa.bassambooking",
       usesAppleSignIn: true
     },
     extra: {
